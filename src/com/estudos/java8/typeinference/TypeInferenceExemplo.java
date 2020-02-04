76e2084a7aebcafc721bbde1629d3c0aab001367
Carlos Sempe <sempejunior@gmail.com>
2020-02-03 18:26:58 -0800

    Type Inference Java 8

---
+++ src/com/estudos/java8/typeinference/TypeInferenceExemplo.java
@@ -0,0 +1,61 @@
+package com.estudos.java8.typeinference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Produto {
+	private String nome;
+	private Double preco;
+	
+	public Produto(String nome, Double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
+
+class ImprimeProdutos {
+	
+	public static void imprime(List<Produto> lista) {
+		if(!lista.isEmpty()) {
+			lista.forEach(p -> System.out.println(p.getNome()));
+		}
+		else {
+			System.out.println("Lista vazia");
+		}
+	}
+}
+
+public class TypeInferenceExemplo {
+
+	public static void main(String[] args) {
+		
+		List<Produto> lista = new ArrayList<>();
+		
+		lista.add(new Produto("TV 42'", 2000.00));
+		lista.add(new Produto("Geladeira 470L'", 3200.00));
+		lista.add(new Produto("Fog�o 4 bocas", 900.00));
+		lista.add(new Produto("Videogame", 1999.00));
+		lista.add(new Produto("Microondas", 550.00));
+		
+		//Infer�ncia de tipo na chamada ao m�todo especializado imprime da classe ImprimeProdutos
+		ImprimeProdutos.imprime(new ArrayList<>());
+		
+	}
+
+}
